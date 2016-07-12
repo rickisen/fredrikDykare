103db8391e1e1d9a95e9442de365009f02d446e0
rickisen <rickard.spam@gmail.com>
2016-07-12 18:53:44 +0200

    Add puffs and reindent

--- src/front-page.php
+++ src/front-page.php
@@ -1,12 +1,20 @@
 <?php get_header(); ?>
-      <?php the_post(); ?>
-      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article">
-        <header class="entry-header">
-          <h1><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-        </header>
-        <div class="entry-content">
-          <?php the_content(); ?>
-          <?php wp_link_pages(); ?>
-        </div>
-      </article>
+  <?php the_post(); ?>
+
+  <article id="frontpage-page" <?php post_class(); ?> role="article">
+    <header class="entry-header">
+      <h1><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+    </header>
+
+    <div id='main-image' class="background-img" style="background-image:url(http://i.imgur.com/Jg3Zjlb.jpg);"></div>
+
+    <div class="entry-content">
+      <?php the_content(); ?>
+      <?php wp_link_pages(); ?>
+    </div>
+  </article>
+  <hr>
+
+  <?php get_template_part('template-parts/puffs')?>
+  <hr>
 <?php get_footer(); ?>
